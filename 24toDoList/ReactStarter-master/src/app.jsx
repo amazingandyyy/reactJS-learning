9424baa24703b3be718950e5b35f35cc9afbc7d2
amazingandyyy <qq830214a@gmail.com>
2016-03-07 00:06:54 -0800

    add reactFire and fireBase
    add reactFire and fireBase

--- 24toDoList/ReactStarter-master/src/app.jsx
+++ 24toDoList/ReactStarter-master/src/app.jsx
@@ -1,13 +1,16 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var reactFire = require('reactfire');
+var Firebase = require('firebase');
 
 var Hello = React.createClass({
   render: function() {
     return <h1 className="red">
-      Hello!
+      Hello yeah!
     </h1>
   }
 });
+
 
 var element = React.createElement(Hello, {});
 ReactDOM.render(element, document.querySelector('.container'));
