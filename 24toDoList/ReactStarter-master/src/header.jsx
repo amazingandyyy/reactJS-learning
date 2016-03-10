d70e7f4100c3a167b20dadba00954c861a2d1188
amazingandyyy <qq830214a@gmail.com>
2016-03-09 20:09:18 -0800

    solve the header
    solve the header

--- 24toDoList/ReactStarter-master/src/header.jsx
+++ 24toDoList/ReactStarter-master/src/header.jsx
@@ -1,14 +1,31 @@
 var React = require('react');
 
-module.experts = React.createClass({
+module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      text: ''
+    }
+  },
   render: function() {
       return <div className='input-group'>
-        <input type='text' className='form-control' />
+        <input
+          value={this.state.text}
+          onChange={this.handleInputChange}
+          type='text' className='form-control' />
         <span className='input-group-btn'>
-          <button className='btn btn-default' type='button'>
+          <button
+            onClick = {this.handleClick}
+            className='btn btn-default'
+            type='button'>
             Add
           </button>
         </span>
       </div>
+  },
+  handleClick: function() {
+    // Send Value of text input to Firebase
+  },
+  handleInputChange: function(event) {
+    event.target
   }
 });
